022ddf5796d132df5c0953d88cfa2259e6a7b0da
Bartosz Kalota <bartek.kalota@wp.pl>
2020-09-18 20:49:18 +0200

    create 'json to object parsing function'

--- common/utils.js
+++ common/utils.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-import { BASIC_HTML_TEMPLATE, RESULT_OUTPUT } from './variables.js';
+import { BASIC_HTML_TEMPLATE, RESULT_OUTPUT, LIST_BASIC_INPUT } from './variables.js';
 
 export const getHTMLTemplate = async (fileName = BASIC_HTML_TEMPLATE) => {
   try {
@@ -18,6 +18,15 @@ export const fillTemplateWithData = (template, title, body) => {
 export const saveOutputHTML = async (dataToSave, fileName = RESULT_OUTPUT) => {
   try {
     await fs.promises.writeFile(fileName, dataToSave);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getInputData = async (fileName = LIST_BASIC_INPUT) => {
+  try {
+    const content = await fs.promises.readFile(fileName, 'utf-8');
+    return JSON.parse(content);
   } catch (err) {
     console.log(err);
   }
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import * as CONSTANTS from './common/variables.js';
-import { getHTMLTemplate, fillTemplateWithData, saveOutputHTML } from './common/utils.js';
+import {
+  getHTMLTemplate, fillTemplateWithData, saveOutputHTML, getInputData
+} from './common/utils.js';
 
 (async () => {
   const template = await getHTMLTemplate();
   const filledTemplate = fillTemplateWithData(template, 'My first generated HTML', 'It\'s quite fun!');
   await saveOutputHTML(filledTemplate);
+  
+  console.log(await getInputData());
 })();
