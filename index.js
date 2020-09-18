47693799ecbd5413369239d59c52172691fbacf3
Bartosz Kalota <bartek.kalota@wp.pl>
2020-09-19 00:10:03 +0200

    Adding command options for page title and list type

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ program
 program
   .command('list')
   .alias('l')
-  .action(async () => {
+  .option('-tp, --type [type]', 'order (ol) or unorder (ul) list, default is ul')
+  .option('-t, --title [title]', 'title for your HTML')
+  .action(async ({ type, title }) => {
     try {
-      await basicListPath();
+      const listTemplate = type === 'ol' ? CONSTANTS.OL_BASIC_TEMPLATE : CONSTANTS.UL_BASIC_TEMPLATE;
+      await basicListPath(listTemplate, title);
     } catch (err) {
       console.log(err);
     }
--- paths/paths.js
+++ paths/paths.js
@@ -6,15 +6,15 @@ import {
 } from '../common/utils.js';
 import { getListTemplate, fillListTemplateWithDataBasic } from '../list/list-utils.js';
 
-export const basicListPath = async () => {
+export const basicListPath = async (listTemplateFile, title = 'Title unset') => {
   try {
     // Fill UL list with fav games
     const parsedListBasicJson = await getInputData();
-    const listTemplate = await getListTemplate();
+    const listTemplate = await getListTemplate(listTemplateFile);
     const completeGameList = fillListTemplateWithDataBasic(listTemplate, parsedListBasicJson);
     // Incorporate filled list into basic HTML template
     const template = await getHTMLTemplate();
-    const filledTemplate = fillTemplateWithData(template, 'My favorite games', completeGameList);
+    const filledTemplate = fillTemplateWithData(template, title, completeGameList);
     await saveOutputHTML(filledTemplate);
   } catch (err) {
     console.log(err);
