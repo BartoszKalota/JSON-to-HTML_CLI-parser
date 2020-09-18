d1000e7bb20d4afb431a74844e8cc3ec34357d73
Bartosz Kalota <bartek.kalota@wp.pl>
2020-09-18 21:56:00 +0200

    Create file implementing all new functions

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 import * as CONSTANTS from './common/variables.js';
-import {
-  getHTMLTemplate, fillTemplateWithData, saveOutputHTML, getInputData
-} from './common/utils.js';
-import { getListTemplate, fillListTemplateWithDataBasic } from './list/list-utils.js';
+import { basicListPath } from './paths/paths.js';
 
 (async () => {
-  const template = await getHTMLTemplate();
-  const filledTemplate = fillTemplateWithData(template, 'My first generated HTML', 'It\'s quite fun!');
-  await saveOutputHTML(filledTemplate);
-  
-  const parsedListBasicJson = await getInputData();
-  const listTemplate = await getListTemplate();
-  console.log(fillListTemplateWithDataBasic(listTemplate, parsedListBasicJson));
+  await basicListPath();
 })();
