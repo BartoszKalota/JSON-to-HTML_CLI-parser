a9720b8f55eb3cfc8ff1c257308d13837d447d08
Bartosz Kalota <bartek.kalota@wp.pl>
2020-09-18 22:27:48 +0200

    Commander installation for launching main function from cli

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+import program from 'commander';
+
 import * as CONSTANTS from './common/variables.js';
 import { basicListPath } from './paths/paths.js';
 
-(async () => {
-  await basicListPath();
-})();
+program
+  .version('1.0.0')
+  .description('JSON to HTML - CLI parser');
+
+program
+  .command('list')
+  .alias('l')
+  .action(async () => {
+    try {
+      await basicListPath();
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+program.parse(process.argv);
